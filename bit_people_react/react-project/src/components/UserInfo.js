359688ca134416831821936f3a50af1c6861093c
BIT Student <classroom@bgit.rs>
2019-04-02 11:30:00 +0200

    feature 7 completed color

--- bit_people_react/react-project/src/components/UserInfo.js
+++ bit_people_react/react-project/src/components/UserInfo.js
@@ -5,12 +5,12 @@ import formatEmail from "./../services/HiddenEmail"
 const UserInfo = (props) => {
     return props.users.map((user, i) => {
 
-        //const color = { backgroundColor : user.gender === "female" ? "pink" : ""}
+        const color = { backgroundColor: user.gender === "female" ? "#ffebee" : "" }
 
         return (
             <div className="card newDiv" >
 
-                <div className="card-body">
+                <div className="card-body" style={color}>
                     <img src={user.picture} className="" alt="..." />
                     <div className="col-lg-5 offset-1">
                         <h5 className="card-title">{user.name} {user.lastName}</h5>
