5dec85593a78387d925a04febc6aa59d44293f01
zoran94 <zorans94@hotmail.com>
2019-07-29 12:58:58 +0200

    Updates

---
+++ precache-manifest.a49cb7e4232711605e144525db20ea97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee63b9063b523f7b2fbb",
+    "url": "/Bit_people/static/css/main.1660e11b.chunk.css"
+  },
+  {
+    "revision": "ee63b9063b523f7b2fbb",
+    "url": "/Bit_people/static/js/main.abea6b93.chunk.js"
+  },
+  {
+    "revision": "208ca1fa431570e8ea22",
+    "url": "/Bit_people/static/js/runtime~main.db3f7261.js"
+  },
+  {
+    "revision": "50dd582546a5a53876bc",
+    "url": "/Bit_people/static/js/2.907e8724.chunk.js"
+  },
+  {
+    "revision": "28ee9fcf93253cc3f6e13f38593b652e",
+    "url": "/Bit_people/index.html"
+  }
+];
